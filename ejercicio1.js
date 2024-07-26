de818e8a2572318a2627d913d6f5290c62cd3f0c
Oscarfp <oscarfp1992@gmail.com>
2024-07-26 11:27:17 +0200

    ok

---
+++ ejercicio1.js
@@ -0,0 +1,23 @@
+//Utiliza un bucle para filtrar personajes de Star Wars por la especie "Human" y guárdales en un nuevo arrray llamado humanCharacters. Imprime este nuevo array por consola.
+
+const characters = [
+  { name: 'Luke Skywalker', species: 'Human' },
+  { name: 'Darth Vader', species: 'Human' },
+  { name: 'Chewbacca', species: 'Wookiee' },
+  { name: 'Leia Organa', species: 'Human' },
+  { name: 'R2-D2', species: 'Droid' },
+  { name: 'C-3PO', species: 'Droid' },
+  { name: 'Obi-Wan Kenobi', species: 'Human' },
+  { name: 'Yoda', species: 'Unknown' },
+  { name: 'Han Solo', species: 'Human' }
+];
+const humanCharacters = [];
+
+// Añade tu código de bucle aquí
+for (i = 0; i < characters.length; i++) {
+  if (characters[i].species == "Human") {
+    humanCharacters.push (characters[i]);
+  }
+}
+
+console.log(humanCharacters);
