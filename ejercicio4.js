4df9b8a0e853e7152a36764b7eccb5abc4772270
Oscarfp <oscarfp1992@gmail.com>
2024-07-26 12:21:31 +0200

    ok

---
+++ ejercicio4.js
@@ -0,0 +1,26 @@
+// Utiliza un bucle para encontrar la serie de dibujos animados más antigua en el array y guarda su nombre en una variable. Imprime el nombre por consola.
+
+const cartoons = [
+  { name: 'Mickey Mouse', debut: 1928 },
+  { name: 'Bugs Bunny', debut: 1938 },
+  { name: 'SpongeBob SquarePants', debut: 1999 },
+  { name: 'Tom and Jerry', debut: 1940 },
+  { name: 'Scooby-Doo', debut: 1969 },
+  { name: 'The Flintstones', debut: 1960 },
+  { name: 'Batman: The Animated Series', debut: 1992 },
+  { name: 'The Simpsons', debut: 1989 },
+  { name: 'Pokémon', debut: 1997 },
+  { name: "Dexter's Laboratory", debut: 1996 }
+];
+
+// Añade tu código de bucle aquí
+let oldestCartoon = cartoons[0].name;
+let oldestDebut = cartoons[0].debut;
+
+for (i = 0; i < cartoons.length; i++) {
+  if (cartoons[i].debut < oldestDebut) {
+    oldestDebut = cartoons[i].debut;
+    oldestCartoon = cartoons[i].name;
+  } 
+}
+console.log (oldestCartoon);
