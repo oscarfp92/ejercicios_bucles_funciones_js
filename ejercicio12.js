33d4fa49c74a2f1438dfc55b1c2c877c9e9b2503
Oscarfp <oscarfp1992@gmail.com>
2024-07-31 13:45:35 +0200

    ok

---
+++ ejercicio12.js
@@ -0,0 +1,26 @@
+//Crea una función que, dada una lista de actores con su año de nacimiento, calcule su edad actual y retorne un nuevo array con los nombres de los actores y sus edades. Averigua como hallar el año actual en tus cálculos.
+
+const actors = [
+  { name: 'Leonardo DiCaprio', born: 1974 },
+  { name: 'Tom Hanks', born: 1956 },
+  { name: 'Meryl Streep', born: 1949 },
+  { name: 'Brad Pitt', born: 1963 },
+  { name: 'Johnny Depp', born: 1963 },
+  { name: 'Scarlett Johansson', born: 1984 },
+  { name: 'Jennifer Lawrence', born: 1990 },
+  { name: 'Denzel Washington', born: 1954 },
+  { name: 'Morgan Freeman', born: 1937 },
+  { name: 'Cate Blanchett', born: 1969 }
+];
+
+function calculateActorsAges(actors) {
+  const result = [];
+  for (i = 0; i < actors.length; i++) {
+  const actor = actors[i];
+  const age = 2024 - actor.born;
+  result.push ({name: actor.name, age: age})
+}
+return result;
+}
+
+console.log (calculateActorsAges(actors))
